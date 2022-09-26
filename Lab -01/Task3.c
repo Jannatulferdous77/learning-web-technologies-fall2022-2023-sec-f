b8c8fd76993c1e66bc059e1f6f88ec6b3471f5b6
jf188991 <jf188991@gmail.com>
2022-09-26 16:53:16 +0600

    Task3

--- Lab -01/Task3.c
+++ Lab -01/Task3.c
@@ -1,1 +1,50 @@
+<JOB_INFORMATION>
+
+	<INFORMATION NO="1">
+
+
+		<NAME>John Doe</NAME>
+		<ADDRESS>
+            <FLAT_NO></FLAT_NO>
+            <HOUSE_NO>H#38</HOUSE_NO>
+            <ROAD_NO>R#3</ROAD_NO>
+            <COLONI>Banani</COLONI>
+            <LOCATION>Dhaka</LOCATION>
+        </ADDRESS>
+		<DEPARTMENT>Sales</DEPARTMENT>
+		<SALARY>20000</SALARY>
+
+	</INFORMATION>
+
+	<INFORMATION NO="2">
+
+
+		<NAME>Mark Zuxkerberg</NAME>
+		<ADDRESS>
+            <FLAT_NO>H#A/1</FLAT_NO>
+            <HOUSE_NO>H#20</HOUSE_NO>
+            <ROAD_NO>R#18</ROAD_NO>
+            <COLONI>Mirpur</COLONI>
+            <LOCATION>Dhaka</LOCATION>
+        </ADDRESS>
+		<DEPARTMENT>Accounts</DEPARTMENT>
+		<SALARY>12000</SALARY>
+
+	</INFORMATION>
+
+	<INFORMATION NO="3">
+
+
+		<NAME>Ala Voski</NAME>
+		<ADDRESS>
+            <FLAT_NO></FLAT_NO>
+            <HOUSE_NO>H#38</HOUSE_NO>
+            <ROAD_NO>R#3</ROAD_NO>
+            <COLONI>Motijhil</COLONI>
+            <LOCATION></LOCATION>
+		<DEPARTMENT>Human Resource</DEPARTMENT>
+		<SALARY>30000</SALARY>
+
+	</INFORMATION>
+</JOB_INFORMATION>
 
